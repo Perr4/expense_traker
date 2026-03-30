a126be7ee33e6205d1181b7e261c5b90e7a68a25
perra <giacomoperra16@gmail.com>
2026-03-30 18:19:58 +0200

    Expense List creation

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,22 +1,43 @@
+import 'package:expense_traker/expense.dart';
 import 'package:flutter/material.dart';
 
-class Expenses extends StatefulWidget{
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
   @override
   State<StatefulWidget> createState() {
     return _ExpensesState();
   }
-  
 }
 
-class _ExpensesState extends State<Expenses>{
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "flutter Curse",
+      amount: 19.99,
+      date: DateTime.now(),
+      tipo: Category.lavoro,
+    ),
+    Expense(
+      title: "Game",
+      amount: 15.30,
+      date: DateTime.now(),
+      tipo: Category.svago,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: const [Text("data"),Text("data")],),
-
+      body: Center(
+        child: Column(
+          
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+        
+          children: [const Text("data"), const Text("data")],
+        ),
+      ),
     );
   }
-
 }
---
+++ lib/expenses_list.dart
@@ -0,0 +1,14 @@
+
+import 'package:expense_traker/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget  {
+
+  final List<Expense> expenses;
+
+  const ExpensesList({super.key, required this.expenses});
+    @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
